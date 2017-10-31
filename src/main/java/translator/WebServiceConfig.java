ef75c9284c69e23e8717f9ce3b89df1b9ed421dd
Xian Pan <xian_pan_123@hotmail.com>
2017-10-31 08:04:36 -0400

    Added SOAP Authentication with Username and Password. Changed logger

--- src/main/java/translator/WebServiceConfig.java
+++ src/main/java/translator/WebServiceConfig.java
@@ -1,5 +1,8 @@
 package translator;
 
+import java.util.List;
+import java.util.Properties;
+
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -7,8 +10,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.server.EndpointInterceptor;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.ws.soap.security.wss4j2.callback.SimplePasswordValidationCallbackHandler;
+import org.springframework.ws.soap.security.wss4j2.Wss4jSecurityInterceptor;
 import org.springframework.xml.xsd.SimpleXsdSchema;
 import org.springframework.xml.xsd.XsdSchema;
 
@@ -37,4 +43,27 @@ public class WebServiceConfig extends WsConfigurerAdapter {
 	public XsdSchema countriesSchema() {
 		return new SimpleXsdSchema(new ClassPathResource("translator.xsd"));
 	}
+	
+    @Bean
+    public SimplePasswordValidationCallbackHandler callbackHandler(){
+        SimplePasswordValidationCallbackHandler callbackHandler = new SimplePasswordValidationCallbackHandler();
+        Properties users = new Properties();
+        users.setProperty("admin", "secret");
+        callbackHandler.setUsers(users);
+        return callbackHandler;
+    }
+    
+    @Bean
+    public Wss4jSecurityInterceptor securityInterceptor() {
+
+    	Wss4jSecurityInterceptor securityInterceptor = new Wss4jSecurityInterceptor();
+    	securityInterceptor.setValidationActions("Timestamp UsernameToken");
+        securityInterceptor.setValidationCallbackHandler(callbackHandler());
+        return securityInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(List<EndpointInterceptor> interceptors) {
+        interceptors.add(securityInterceptor());
+    }
 }
--- src/test/java/translator/web/ws/TranslatorEndpointTest.java
+++ src/test/java/translator/web/ws/TranslatorEndpointTest.java
@@ -6,10 +6,15 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.context.annotation.Bean;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.ClassUtils;
 import org.springframework.ws.client.core.WebServiceTemplate;
+import org.springframework.ws.client.support.interceptor.ClientInterceptor;
+import org.springframework.ws.soap.client.SoapFaultClientException;
+import org.springframework.ws.soap.security.wss4j2.Wss4jSecurityInterceptor;
+
 import translator.Application;
 import translator.web.ws.schema.GetTranslationRequest;
 import translator.web.ws.schema.GetTranslationResponse;
@@ -34,18 +39,53 @@ public class TranslatorEndpointTest {
 		marshaller.setPackagesToScan(ClassUtils.getPackageName(GetTranslationRequest.class));
 		marshaller.afterPropertiesSet();
 	}
-
+	
+	public Wss4jSecurityInterceptor securityInterceptor1(){
+        Wss4jSecurityInterceptor wss4jSecurityInterceptor = new Wss4jSecurityInterceptor();
+        wss4jSecurityInterceptor.setSecurementActions("Timestamp UsernameToken");
+        wss4jSecurityInterceptor.setSecurementUsername("admin");
+        wss4jSecurityInterceptor.setSecurementPassword("secret");
+        return wss4jSecurityInterceptor;
+    }
+	
+	public Wss4jSecurityInterceptor securityInterceptor2(){
+        Wss4jSecurityInterceptor wss4jSecurityInterceptor = new Wss4jSecurityInterceptor();
+        wss4jSecurityInterceptor.setSecurementActions("Timestamp UsernameToken");
+        wss4jSecurityInterceptor.setSecurementUsername("admin");
+        wss4jSecurityInterceptor.setSecurementPassword("secrete");
+        return wss4jSecurityInterceptor;
+    }
+	
 	@Test
-	public void testSendAndReceive() {
+	public void testSendAndReceiveOK() {
 		GetTranslationRequest request = new GetTranslationRequest();
 		request.setLangFrom("zh");
 		request.setLangTo("en");
 		request.setText("这是一个试验的翻译服务");
-		Object response = new WebServiceTemplate(marshaller).marshalSendAndReceive("http://localhost:"
+		ClientInterceptor[] interceptors = new ClientInterceptor[] {securityInterceptor1()};
+		
+		WebServiceTemplate webService = new WebServiceTemplate(marshaller);
+		webService.setInterceptors(interceptors);
+		Object response = webService. marshalSendAndReceive("http://localhost:"
 				+ port + "/ws", request);
 		assertNotNull(response);
 		assertThat(response, instanceOf(GetTranslationResponse.class));
 		GetTranslationResponse translation = (GetTranslationResponse) response;
 		assertThat(translation.getTranslation(), is("This is a test of the translation service"));
+	}	
+	
+	
+	@Test(expected=SoapFaultClientException.class)
+	public void testSendAndReceiveBAD() {
+		GetTranslationRequest request = new GetTranslationRequest();
+		request.setLangFrom("zh");
+		request.setLangTo("en");
+		request.setText("这是一个试验的翻译服务");
+		ClientInterceptor[] interceptors = new ClientInterceptor[] {securityInterceptor2()};
+		
+		WebServiceTemplate webService = new WebServiceTemplate(marshaller);
+		webService.setInterceptors(interceptors);
+		webService. marshalSendAndReceive("http://localhost:"
+				+ port + "/ws", request);
 	}	
 }
